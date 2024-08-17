01d0c0da917da19bbc9b2dcf1082da9542af777b
Aman Panwar <amanpanwar1672@gmail.com>
2024-08-17 19:14:41 +0530

    Added a File upload cloud button from React-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import Model from "./Model";
+import image1 from "/models/yaskawapng.png";
+import { MdCloudUpload } from "react-icons/md";
 
 const App = () => {
   return (
-   <Model/>
+    <>
+      <div className="text-[40px] ">
+        <img src={image1} alt="" className="mt-6 ml-8 mb-6 h-[40px]" />
+      </div>
+      <div className="text-[50px] text-center font-bold ">SELECT YOUR MODEL</div>
+      <div className="flex ml-8 mt-4">
+        <div className="text-[30px] font-semibold w-1/3 text-center">
+      
+        <form action="" className="flex flex-col justify-center mt-14 ml-2 items-center border-[2px] border-dashed border-[#1475cf] h-[300px] w-[500px] cursor-pointer rounded-[5px]" onClick={()=>document.querySelector(".input-field").click()}>
+          <input type="file" className="input-field ml-[290px] " hidden/>
+          <MdCloudUpload color="#1475cf" size={120}/>
+          <div className="text-3xl ">Upload File</div>
+        </form>
+
+        </div>
+        {/* <div className="h-[580px] bg-black w-[2px] ml-10"></div> */}
+      </div>
+    </>
   );
 };
 
--- src/Model.jsx
+++ src/Model.jsx
@@ -9,7 +9,7 @@ const Model = () => {
   return (
     <>
     <div className="text-[40px]  font-semibold m-0">
-      <img src={image1} alt="" className="mt-6 ml-8 mb-6 h-[40px]"  />
+      <img src={image1} alt="Yaskawa" className="mt-6 ml-8 mb-6 h-[40px]"  />
     </div>
     <div className="flex gap-0 p-0 overflow-hidden mr-6">
       <div className="w-3/4 text-3xl mt-5 ml-10 border-0 h-full border-solid border-black rounded overflow-hidden">
@@ -28,7 +28,7 @@ const Model = () => {
       <div className="flex items-center justify-between">
       <h1 className="ml-6 mt-2 font-semibold">© Developed and maintain by Aman Panwar</h1>
       <div>
-      <a href="https://github.com/amanxxp/Yaskawa.Project" className="mr-8 font-bold text-blue-800">Github Repo</a>
+      <a href="https://github.com/amanxxp/Yaskawa.Project" target='_main' className="mr-8 font-bold text-blue-800">Github Repo</a>
       </div>
       </div>
     </div>
